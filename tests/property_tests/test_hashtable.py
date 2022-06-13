b0dd8fa4b236c65acee9abd7c5419d1188d0ce84
Knut Rand <knutdrand@gmail.com>
2022-06-13 14:28:07 +0200

    Fixed dtype-bug in hashtable tests

--- tests/property_tests/test_hashtable.py
+++ tests/property_tests/test_hashtable.py
@@ -1,10 +1,10 @@
 import pytest
+from numpy import array, int8, int16, int32, int64
 from numpy.testing import assert_equal
 from npstructures import HashTable
 import hypothesis.extra.numpy as stnp
 from hypothesis import given, example
 from .strategies import matrix_and_indexes, matrices, nested_lists, arrays, array_shapes, two_nested_lists, integers
-import hypothesis.strategies as st
 from hypothesis.strategies import composite
 
 
@@ -53,11 +53,14 @@ def test_lookup(data, mod):
 
 @pytest.mark.parametrize("mod", modulos)
 @given(data=hashtable_keys_and_values(), single_value=integers())
+@example(data=(array([0], dtype=int8), array([0], dtype=int8)),
+         single_value=128,
+         mod=None)
 def test_set_all_to_single_value(data, single_value, mod):
     keys, values = data
     h = HashTable(keys, values, mod=mod)
     h[keys] = single_value
-    assert_equal(h[keys], single_value)
+    assert_equal(h[keys], values.dtype.type(single_value))
 
 
 @pytest.mark.parametrize("mod", modulos)
@@ -68,7 +71,7 @@ def test_set_some_to_single_value(data, single_value, mod):
     # change half of the values
     change_keys = keys[::2]
     h[change_keys] = single_value
-    assert_equal(h[change_keys], single_value)
+    assert_equal(h[change_keys], values.dtype.type(single_value))
 
 
 @pytest.mark.parametrize("mod", modulos)
