f70444af44d5d2da61ffaf282136663745ec1d6e
Knut Rand <knutdrand@gmail.com>
2022-09-28 15:55:39 +0200

    Real testing

--- npstructures/testing.py
+++ npstructures/testing.py
@@ -1,6 +1,31 @@
+import numpy as np
+from .raggedarray import RaggedArray
+import dataclasses
+
+
+def shallow_tuple(self):
+    return tuple(
+        getattr(self, field.name) for field in dataclasses.fields(self)
+    )
+
+
 def assert_npdataclass_equal(a, b):
-    pass
+    print(a)
+    print(b)
+    for s, o in zip(shallow_tuple(a), shallow_tuple(b)):
+        assert hasattr(s, "shape") and hasattr(o, "shape"), (s, o)
+        if not s.shape == o.shape:
+            assert False, (s.shape, o.shape, str(a), str(b))
+        if not np.all(np.equal(s, o)):
+            assert False, (a, b)
 
 
 def assert_raggedarray_equal(a, b):
-    pass
+    if isinstance(a, list):
+        a = RaggedArray(a)
+
+    if isinstance(b, list):
+        b = RaggedArray(b)
+
+    assert a.shape == b.shape, (a, b)
+    assert np.all(a.ravel() == b.ravel()), (a, b)
