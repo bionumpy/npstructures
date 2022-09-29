7aaac21a8bbc4cdea1a711b845fd78b1d23f90c6
Knut Rand <knutdrand@gmail.com>
2022-09-29 10:04:07 +0200

    ragged_slice for array

--- npstructures/raggedarray/raggedslice.py
+++ npstructures/raggedarray/raggedslice.py
@@ -3,15 +3,26 @@ import numpy as np
 
 
 def ragged_slice(array, starts=None, ends=None):
-    assert isinstance(array, RaggedArray)
+    if isinstance(array, RaggedArray):
+        base_starts = array.shape.starts
+        base_ends = array.shape.ends
+    else:
+        assert isinstance(array, np.ndarray)
+        if len(array.shape) == 1:
+            base_starts = 0
+            base_ends = array.size
+        else:
+            assert len(array.shape) == 2
+            base_starts = np.arange(len(array))*array.shape[-1]
+            base_ends = base_starts+array.shape[-1]
     if starts is None:
-        starts = array.shape.starts
+        starts = base_starts
     else:
-        starts = array.shape.starts+starts
+        starts = base_starts+starts
     if ends is None:
-        ends = array.shape.ends
+        ends = base_ends
     else:
-        ends = np.where(ends < 0, array.shape.ends+ends, array.shape.starts+ends)
+        ends = np.where(ends < 0, base_ends+ends, base_starts+ends)
     lengths = ends-starts
     indices, shape = RaggedView(starts, lengths).get_flat_indices()
     return array.__class__(array.ravel()[indices], shape)
