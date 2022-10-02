cc56ea5a4b7fa2a9f9a183eebd01db015b59869f
Knut Rand <knutdrand@gmail.com>
2022-10-02 09:48:38 +0200

    Add method to rla

--- npstructures/runlengtharray.py
+++ npstructures/runlengtharray.py
@@ -3,10 +3,16 @@ from .util import unsafe_extend_left, unsafe_extend_right
 
 
 class RunLengthArray:
-    def __init__(self, starts, ends, values):
-        self._starts = starts
-        self._ends = ends
+    def __init__(self, events, values):
+        assert events[0] == 0, events
+        assert len(events)==len(values)+1, (events, values)
+        self._events = events
+        self._starts = events[:-1]
+        self._ends = events[1:]
         self._values = values
+
+    def __len__(self):
+        return self._ends[-1]
 
     @classmethod
     def from_array(cls, array):
@@ -14,12 +20,8 @@ class RunLengthArray:
         mask = unsafe_extend_left(array) != unsafe_extend_right(array)
         mask[0], mask[-1] = (True, True)
         indices = np.flatnonzero(mask)
-        starts, ends = (indices[:-1], indices[1:])
-        values = array[starts]
-        return cls(starts, ends, values)
-
-    def __len__(self):
-        return self._ends[-1]
+        values = array[indices[:-1]]
+        return cls(indices, values)
 
     def to_array(self):
         array = np.zeros_like(self._values, shape=len(self))
@@ -28,3 +30,22 @@ class RunLengthArray:
         array[self._starts] = diffs
         np.cumsum(array, out=array)
         return array
+
+    def __add__(self, other):
+        assert len(self) == len(other), (self, other)
+        all_events = np.concatenate([self._starts, other._starts])
+        args = np.argsort(all_events, kind="mergesort")
+        all_events = all_events[args]
+        values = np.zeros((2, len(all_events)), dtype=self._values.dtype)
+        other_d = np.diff(unsafe_extend_left(other._values))
+        other_d[0] = other._values[0]
+        my_d = np.diff(unsafe_extend_left(self._values))
+        my_d[0] = self._values[0]
+        values[1, args >= self._starts.size] = other_d
+        values[0, args < self._starts.size] = my_d
+        np.cumsum(values, axis=-1, out=values)
+        sum_values = values[0]+values[1]
+        rm_idxs = np.flatnonzero(all_events[:-1] == all_events[1:])
+        all_events = np.delete(all_events, rm_idxs)
+        sum_values = np.delete(sum_values, rm_idxs)
+        return self.__class__(np.append(all_events, self._events[-1]), sum_values)
--- tests/test_runlengtharray.py
+++ tests/test_runlengtharray.py
@@ -1,14 +1,23 @@
 from npstructures.runlengtharray import RunLengthArray
 from numpy.testing import assert_array_equal
+import numpy as np
 import pytest
 
+arrays = [[0, 1, 1, 2, 3, 3, 3],
+          [0, 20, 1, 1, 0, 3, 3],
+          [2, 2, 2, 1],
+          [1],
+          [2.3, 19, 32.7, 32.7]]
 
-@pytest.fixture
-def array():
-    return [0, 1, 1, 2, 3, 3, 3]
 
-
+@pytest.mark.parametrize("array", arrays)
 def test_run_length_array(array):
     rlarray = RunLengthArray.from_array(array)
     new_array = rlarray.to_array()
     assert_array_equal(array, new_array)
+
+
+def test_add_run_length_array():
+    array1, array2 = (np.asanyarray(a) for a in arrays[:2])
+    rl_result = RunLengthArray.from_array(array1)+RunLengthArray.from_array(array2)
+    assert_array_equal(rl_result.to_array(), array1+array2)
