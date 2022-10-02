b09de24f72fd64e9cc5129749a0471ee6ac83981
Knut Rand <knutdrand@gmail.com>
2022-10-02 10:16:54 +0200

    float64 and float32 supported

--- npstructures/runlengtharray.py
+++ npstructures/runlengtharray.py
@@ -37,22 +37,24 @@ class RunLengthArray:
         args = np.argsort(all_events, kind="mergesort")
         all_events = all_events[args]
 
-        # other_d = np.diff(unsafe_extend_left(other._values))
-        dtype = np.uint64 if other._values.dtype == np.float64 else other._values.dtype
-        o_values = other._values.view(dtype)
-        dtype = np.uint64 if self._values.dtype == np.float64 else self._values.dtype
-
-        m_values = self._values.view(dtype)
-        other_d = unsafe_extend_left(o_values[:-1]) ^ o_values.view(dtype)
+        result_dtype = np.result_type(*(i.dtype for i in (self._values, other._values)))
+        temp_dtype = result_dtype
+        if result_dtype == np.float64:
+            temp_dtype = np.uint64
+        elif result_dtype == np.float32:
+            temp_dtype = np.uint32
+        o_values = other._values.astype(result_dtype).view(temp_dtype)
+        m_values = self._values.astype(result_dtype).view(temp_dtype)
+        other_d = unsafe_extend_left(o_values[:-1]) ^ o_values
         other_d[0] = o_values[0]
-        values = np.zeros((2, len(all_events)), dtype=dtype)
-        # my_d = np.diff(unsafe_extend_left(self._values))
+        values = np.zeros((2, len(all_events)), dtype=temp_dtype)
+
         my_d = unsafe_extend_left(m_values)[:-1] ^ m_values
         my_d[0] = m_values[0]
         values[1, args >= self._starts.size] = other_d
         values[0, args < self._starts.size] = my_d
         np.bitwise_xor.accumulate(values, axis=-1, out=values)
-        values = values.view(other._values.dtype)
+        values = values.view(result_dtype)
         sum_values = values[0]+values[1]
         rm_idxs = np.flatnonzero(all_events[:-1] == all_events[1:])
         all_events = np.delete(all_events, rm_idxs)
