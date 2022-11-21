f1b7531c798ba3ae174b510a47010b3ade565a2a
kanduric <chakri.co@gmail.com>
2022-11-21 15:49:42 +0100

    removed as_strided things to avoid segfaults

--- npstructures/util.py
+++ npstructures/util.py
@@ -19,22 +19,26 @@ def as_strided(array, shape=None, strides=None, **kwargs):
 
 def unsafe_extend_right(array, n=1):
     assert len(array.shape) == 1, array.shape
-    return as_strided(array, shape=(array.size+n, ), writeable=False, subok=True)
+    return np.append(array, np.zeros_like(array, shape=(n, )))
+    # return as_strided(array, shape=(array.size+n, ), writeable=False, subok=True)
 
 
 def unsafe_extend_right_2d(array, n=1):
     assert len(array.shape) == 2, array.shape
-    return as_strided(array, shape=(array.shape[0], array.shape[1]+n), writeable=False, subok=True)
+    return np.concatenate((array, np.zeros_like(array, shape=(array.shape[0], n))), axis=-1)
+    # return as_strided(array, shape=(array.shape[0], array.shape[1]+n), writeable=False, subok=True)
 
 
 def unsafe_extend_left(array, n=1):
     assert len(array.shape) == 1, array.shape
-    return unsafe_extend_right(array[::-1], n)[::-1]
+    return np.insert(array, 0, np.zeros_like(array, shape=(n,)))
+    # return unsafe_extend_right(array[::-1], n)[::-1]
 
 
 def unsafe_extend_left_2d(array, n=1):
     assert len(array.shape) == 2
-    return unsafe_extend_right_2d(array[:, ::-1], n)[:, ::-1]
+    return np.concatenate((np.zeros_like(array, shape=(array.shape[0], n)), array), axis=-1)
+    # return unsafe_extend_right_2d(array[:, ::-1], n)[:, ::-1]
 
 
 def bincount2d(array):
