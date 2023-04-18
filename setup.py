024c009bde540892a002ade9226997ee3552afc1
BumpVersion Action <bumpversion@github-actions>
2023-04-18 10:32:38 +0000

    Bump version: 0.2.11 → 0.2.12

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/knutdrand/npstructures',
-    version='0.2.11',
+    version='0.2.12',
     zip_safe=False,
 )
 
