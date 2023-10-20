7ccf174916f49b9a4d91a87e011afe2c73c2690f
Knut Rand <knutdrand@gmail.com>
2023-10-20 14:45:37 +0200

    removing c code test

--- tests/test_c_code.py
+++ tests/test_c_code.py
@@ -1,10 +1,12 @@
 import numpy as np
+import pytest
 from numpy.testing import assert_array_equal
 
-from npstructures.copy_segment import compute
+
+# from npstructures.copy_segment import compute
 print('-----------------------')
 
-
+@pytest.mark.skip
 def test_copy():
     array_1 = np.array([1, 2, 3, 4, 5, 6, 7, 8])
     array_2 = np.zeros_like(array_1, shape=(5,))
