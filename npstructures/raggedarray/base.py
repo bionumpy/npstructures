dad4297522750e4751690b3401bb5a640ed421ab
Knut Rand <knutdrand@gmail.com>
2023-10-23 11:32:28 +0200

    cleaning

--- npstructures/raggedarray/base.py
+++ npstructures/raggedarray/base.py
@@ -40,21 +40,21 @@ class RaggedBase:
         return ret
 
     def _flatten_myself(self):
-        assert not self.is_contigous
+        #assert not self.is_contigous
         if not isinstance(self._shape, (RaggedView2, RaggedView)):
             return
-        if False and isinstance(self._shape, RaggedView2) and self._shape.n_rows>0 and any(self.dtype==d for d in (np.uint8, np.int32, np.int64, np.uint64, np.float64)):
-            shape = self._shape.get_shape()
-            if self.__data.size == 0:
-                pass
-            elif self._shape.col_step == 1 and isinstance(self.__data, np.ndarray) and np.issubdtype(self.__data.dtype, np.integer):
-                self.__data = c_extract_segments(self.__data, self._shape, shape, self._shape.col_step)
-            else:
-                self.__data = native_extract_segments(self.__data, self._shape, shape, self._shape.col_step)
-            self._shape = shape
-            assert isinstance(shape, RaggedShape)
-            self.is_contigous = True
-            return
+        # if False and isinstance(self._shape, RaggedView2) and self._shape.n_rows>0 and any(self.dtype==d for d in (np.uint8, np.int32, np.int64, np.uint64, np.float64)):
+        #     shape = self._shape.get_shape()
+        #     if self.__data.size == 0:
+        #         pass
+        #     elif self._shape.col_step == 1 and isinstance(self.__data, np.ndarray) and np.issubdtype(self.__data.dtype, np.integer):
+        #         self.__data = c_extract_segments(self.__data, self._shape, shape, self._shape.col_step)
+        #     else:
+        #         self.__data = native_extract_segments(self.__data, self._shape, shape, self._shape.col_step)
+        #     self._shape = shape
+        #     assert isinstance(shape, RaggedShape)
+        #     self.is_contigous = True
+        #     return
         idx, shape = self._shape.get_flat_indices()
         self.__data = self.__data[idx]
         self._shape = shape
