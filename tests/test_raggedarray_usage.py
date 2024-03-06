f5e075fb09ecab0f1abc5fbe95078be75a829d92
Knut Rand <knutdrand@gmail.com>
2024-03-06 09:23:46 +0100

    fix: Issue #15

--- tests/test_raggedarray_usage.py
+++ tests/test_raggedarray_usage.py
@@ -3,7 +3,7 @@ import pytest
 from npstructures import RaggedArray
 
 # this works
-@pytest.mark.xfail
+
 def test_assert_raises_value_error():
     with pytest.raises(ValueError):
         x = RaggedArray([0, 1, 2, 3], [4, 5, 6])
