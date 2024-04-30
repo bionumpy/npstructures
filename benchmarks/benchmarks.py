4e2d631e8b5bece983e2df827c61cf293c4a4c6e
Ivar Grytten <ivar.grytten@gmail.com>
2024-04-30 15:56:45 +0200

    intial asv benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,3 +1,9 @@
+import numpy as np
+
+import npstructures as nps
+from npstructures import RaggedArray
+
+
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
@@ -29,3 +35,37 @@ class TimeSuite:
 class MemSuite:
     def mem_list(self):
         return [0] * 256
+
+
+class TimeRaggedArraySuite:
+    def setup(self):
+        np.random.seed(1)
+        self.matrix = np.random.randint(0, 1000, (1000, 1000), dtype=int)
+        self.ragged_array = RaggedArray.from_numpy_array(self.matrix)
+        self.indexes = np.random.randint(0, 1000, 1000)
+
+    def time_nps_row_sum(self):
+        self.ragged_array.sum(axis=-1)
+
+    def time_np_row_sum(self):
+        self.matrix.sum(axis=-1)
+
+    def time_nps_square(self):
+        self.ragged_array ** 2
+
+    def time_np_square(self):
+        self.matrix ** 2
+
+    def time_nps_getitem_row(self):
+        self.ragged_array[self.indexes]
+
+    def time_np_getitem_row(self):
+        self.matrix[self.indexes]
+
+    def time_nps_getitem_col(self):
+        self.ragged_array[:, self.indexes]
+
+    def time_np_getitem_col(self):
+        self.matrix[:, self.indexes]
+
+
